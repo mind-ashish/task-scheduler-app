653b39c7f24bba598da7b32218b9a363352b0a72
Aashish <aashish01official@gmail.com>
2019-07-07 23:50:05 +0530

    setting up the ejs view engine

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,7 @@
 module.exports.home=function(req,res){
     return res.render('home',
     {
-        
+        title:"to-do app"
     }
     );
 }
--- server.js
+++ server.js
@@ -2,7 +2,14 @@ const express=require('express');
 const app=express();
 const port=8000;
 
+//set static files
+app.use(express.static('./assets'));
+
 app.use('/',require('./routes/index'));
+
+//set view engine
+app.set('view engine','ejs');
+app.set('views','./views');
 
 app.listen(port,function(err){
     if(err){
