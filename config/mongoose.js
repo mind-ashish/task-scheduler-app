8b8727347db106d133d010d92a90239dca2abe2e
Aashish <aashish01official@gmail.com>
2019-07-09 02:32:51 +0530

    setting up the database

---
+++ config/mongoose.js
@@ -0,0 +1,9 @@
+const mongoose=require('mongoose');
+mongoose.connect('mongodb://localhost/task_db'); //task_db is name of db
+
+const db=mongoose.connection;
+db.on('error',console.error.bind(console,'error connecting to database'));
+
+db.once('open',function(){
+    console.log('succesfully connected to db');
+});
