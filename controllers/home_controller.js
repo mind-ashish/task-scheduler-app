e48934f9e35cfd646ace5b5b1eaa3ce6e16fb22e
Aashish <aashish01official@gmail.com>
2019-07-18 00:33:41 +0530

    fixing the deletion feature

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -38,14 +38,15 @@ module.exports.add=function(req,res){
 }
 
 module.exports.delete=function(req,res){
-   let ids= req.body.task;
-   for(let i of ids){
-       Task.findByIdAndDelete(i,function(err){
+   let ids= req.body.task;  //can one or more than one ids
+       Task.deleteMany({_id:{$in:ids}},function(err){   //to delete multiple
+        //query has fieldname= _id which is taking values from array ids. So basically finding documents/objects whose _id matches with any element of 
+        //array ids , and deleting that document in mongodb.
         if(err){
             console.log('cannot delete tasks');
             return;
         }
+        return res.redirect('back');
        });
-   }
-   return res.redirect('back');
+   
 }
